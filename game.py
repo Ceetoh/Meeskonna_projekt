5d1407cc099116f29ee5a437e4f60af7960b06d9
Ceetoh <martin.pihlapuu@voco.ee>
2024-05-24 18:20:19 +0300

    As a player i can see grid[Delivers #187667037]

--- game.py
+++ game.py
@@ -2,27 +2,42 @@
 
 
 import pygame
+import sys
+import random
 from settings import Settings
 
 pygame.init()
+
+BLOCK_SIZE = 50
+FONT = pygame.font.Font("font.ttf", BLOCK_SIZE*2)
+
+
+
 gm_settings = Settings()
 
-
+SW, SH = 800, 800
 
 
 screen = pygame.display.set_mode([gm_settings.screen_width, gm_settings.screen_height])
 pygame.display.set_caption(gm_settings.caption)
+clock = pygame.time.Clock()
+
+def drawgrid():
+    for x in range(0, SW, BLOCK_SIZE):
+        for y in range(0, SH, BLOCK_SIZE):
+            rect = pygame.Rect(x, y, BLOCK_SIZE, BLOCK_SIZE)
+            pygame.draw.rect(screen, "#ffffff", rect, 1)
+
+drawgrid()
 
 running = True
 while running:
-    screen.fill(gm_settings.bg_color)
+#    screen.fill(gm_settings.bg_color)
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
+            pygame.quit()
+            sys.exit()
             
-            
-        pygame.display.flip()
-        
-        
-pygame.quit()
+    pygame.display.update()
+    clock.tick(10)
--- settings.py
+++ settings.py
@@ -6,6 +6,6 @@ class Settings:
         
         
         self.screen_width = 800
-        self.screen_height = 600
-        self.bg_color = (3, 252, 102)
+        self.screen_height = 800
+#        self.bg_color = (3, 252, 102)
         self.caption = 'Snake game'
