9a661be8adceb06f9c13c02be499899aca23ccf1
Ceetoh <martin.pihlapuu@voco.ee>
2024-05-24 17:37:24 +0300

    As a player i can see the game window [Delivers #187666276]

--- game.py
+++ game.py
@@ -2,15 +2,20 @@
 
 
 import pygame
+from settings import Settings
+
 pygame.init()
+gm_settings = Settings()
 
 
-screen = pygame.display.set_mode([800, 600])
 
+
+screen = pygame.display.set_mode([gm_settings.screen_width, gm_settings.screen_height])
+pygame.display.set_caption(gm_settings.caption)
 
 running = True
 while running:
-    
+    screen.fill(gm_settings.bg_color)
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
---
+++ settings.py
@@ -0,0 +1,11 @@
+class Settings:
+    
+    
+    
+    def __init__(self):
+        
+        
+        self.screen_width = 800
+        self.screen_height = 600
+        self.bg_color = (3, 252, 102)
+        self.caption = 'Snake game'
