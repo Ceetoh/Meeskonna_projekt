294c40b41cdab81b9eb967749587946fe84a2991
Ceetoh <150243756+Ceetoh@users.noreply.github.com>
2024-05-29 19:09:29 +0300

    "Added Wasd keys, Moved functions into seperate file"

--- settings.py
+++ settings.py
@@ -1,11 +1,6 @@
 class Settings:
-    
-    
-    
     def __init__(self):
-        
-        
         self.screen_width = 800
         self.screen_height = 800
-        self.bg_color = (3, 252, 102)
+        self.bg_color = (3, 252, 152)
         self.caption = 'Snake game'
